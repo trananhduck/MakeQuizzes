8b949fc3662fe11aa1f464538493286ca5761b06
trananhduck <anhduc9b1cva@gmail.com>
2024-12-16 04:42:34 +0700

    xóa file main.mainjs

--- js/main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    QuizHandler.renderQuiz();
-});
-document.getElementById("backToCreateQuestions").addEventListener("click", function () {
-    QuizHandler.resetQuiz(); 
-});
